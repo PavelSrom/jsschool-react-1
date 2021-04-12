e0d8382e144fae111ef47cf8f65503541795ce3f
Pavel Srom <srompavel98@gmail.com>
2021-04-12 22:55:19 +0200

    fix(api-url): https protocol

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,6 @@ import 'antd/dist/antd.css'
 import { App } from './App'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://jsonplaceholder.typicode.com'
+axios.defaults.baseURL = 'https://jsonplaceholder.typicode.com'
 
 render(<App />, document.getElementById('root'))
